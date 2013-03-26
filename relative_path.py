d0a4d72c2b42e373a5bce7a6d5a8a1ea365b023b
unknown <kirr@kirr-v-w7.ld.yandex.ru>
2013-03-26 18:46:53 +0400

    Added command "Copy Relative File Path"

---
+++ relative_path.py
@@ -0,0 +1,16 @@
+import sublime, sublime_plugin
+
+def get_relative_path(file_path, folder_list):
+  for folder in folder_list:
+    if file_path.startswith(folder):
+      file_path = '.' + file_path[len(folder):]
+      break
+  return file_path
+
+class CopyRelativeFilePathCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    active_window = sublime.active_window()
+    folder_list = [] if active_window is None else active_window.folders()
+    file_path = get_relative_path(self.view.file_name(), folder_list)
+    file_path = file_path.replace('\\', '/')
+    sublime.set_clipboard(file_path)
