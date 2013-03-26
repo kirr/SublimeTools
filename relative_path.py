8780632961d2aff3c176fe7d3eb819e9be5d24ea
Kirill Kosarev <kirr@yandex-team.ru>
2013-03-26 19:20:07 +0400

    fixes

--- relative_path.py
+++ relative_path.py
@@ -1,16 +1,19 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
+
 
 def get_relative_path(file_path, folder_list):
-  for folder in folder_list:
-    if file_path.startswith(folder):
-      file_path = '.' + file_path[len(folder):]
-      break
-  return file_path
+    for folder in folder_list:
+        if file_path.startswith(folder):
+            file_path = '.' + file_path[len(folder):]
+            break
+    return file_path
+
 
 class CopyRelativeFilePathCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
-    active_window = sublime.active_window()
-    folder_list = [] if active_window is None else active_window.folders()
-    file_path = get_relative_path(self.view.file_name(), folder_list)
-    file_path = file_path.replace('\\', '/')
-    sublime.set_clipboard(file_path)
+    def run(self, edit):
+        active_window = sublime.active_window()
+        folder_list = [] if active_window is None else active_window.folders()
+        file_path = get_relative_path(self.view.file_name(), folder_list)
+        file_path = file_path.replace('\\', '/')
+        sublime.set_clipboard(file_path)
